5e0e8e6b6e080e8012d0096546473d8dba1b79aa
connor worthen <connorw220@protonmail.com>
2019-11-10 22:39:48 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,6 +1,6 @@
 require 'pry'
 
-   This is the array we will be passing into the remove_strawberry method
+  
   contacts = {
      "Jon Snow" => {
        name: "Jon",
@@ -15,5 +15,5 @@ require 'pry'
    }
 
 def remove_strawberry(contacts)
-  
+
 end
